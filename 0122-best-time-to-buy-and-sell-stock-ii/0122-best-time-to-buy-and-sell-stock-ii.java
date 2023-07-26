1ecc57ec54cb16707b079afcfce7e5aff5a4dec0
Gowri Arunsha <59527493+gowriarunsha@users.noreply.github.com>
2023-07-26 12:30:04 +0530

    Time: 84 ms (7.64%), Space: 44.5 MB (56.90%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,37 +1,43 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int min=Integer.MAX_VALUE;
-        int profit=0;
-        int profittoday=0;
-        int total=0;
+        int min=Integer.MAX_VALUE;  //buying price - minimized
+        int profit=0;   //profit made for stock - maximized
+        //int profittoday=0;  //profit made if sold today
+        int total=0;    //total profit made
         
         for(int i=0;i<prices.length;i++)
         {
+            //NEW DAY
             System.out.println("NEW DAY");
-            if(prices[i]<min)
+            if(prices[i]<min || prices[i]-min<profit)
             {
+                System.out.println("Buy new stock");
+                //BUY STOCK AT new min price
                 min=prices[i];
-                System.out.println("BUY STOCK AT:"+min);
+                total+=profit;
+                profit=0;
             }
             
-            profittoday=prices[i]-min;
-            System.out.println("should we sell at"+prices[i]+"? for profit"+profittoday+"?");
-            
-            //if(sold<prices[i] && profittoday>profit)
-            if(profittoday>profit)
+            //if profit is higher if sold today - 
+            if(prices[i]-min>profit)
             {
                 System.out.println("YES new highest price");
-                total+=profittoday-profit;
-                profit=profittoday;
+                //total+=profittoday-profit;
+                profit=prices[i]-min;
                 System.out.println("profit made:"+profit);
             }
+            /*
             else{
+                total+=profit;
                 System.out.println("Dont sell. Buy stock");
                 min=prices[i];
                 profit=0;
             }
+            */
         }
+        
+        total+=profit;
         return total;
     }
 }
